52c46857a7343c8b6a37f56be83e3170c76dde44
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2017-08-14 14:07:39 +0300

    Task reducer function composition

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,36 +1,44 @@
 import {ADD_TASK, DELETE_TASK, EDIT_TASK, TOGGLE_TASK} from '../actions/index';
 
-export default function reducer(state = [], action) {
+function taskReducer(state = {}, action) {
     switch (action.type) {
         case ADD_TASK:
-            const task = {
+            return {
                 id: action.id,
                 title: action.title,
                 completed: false
             };
-            return [...state, task];
+        case TOGGLE_TASK:
+            if (state.id !== action.id) {
+                return state;
+            }
+            return Object.assign({}, state, {
+                completed: !state.completed
+            });
+
+        case EDIT_TASK:
+            if (state.id !== action.id) {
+                return state;
+            }
+            return Object.assign({}, state, {
+                title: action.title
+            });
+        default :
+            return [];
+    }
+}
+
+export default function reducer(state = [], action) {
+    switch (action.type) {
+        case ADD_TASK:
+            return [...state, taskReducer(undefined, action)];
         case DELETE_TASK:
             const index = state.findIndex(task => task.id === action.id);
             return [...state.slice(0, index), ...state.slice(index + 1)];
         case EDIT_TASK:
-            return state.map(task => {
-                if (task.id !== action.id) {
-                    return task;
-                }
-                return Object.assign({}, task, {
-                    completed: !task.completed
-                })
-            });
-            return [];
+            return state.map(task => taskReducer(task, action));
         case TOGGLE_TASK:
-            return state.map(task => {
-                if (task.id !== action.id) {
-                    return task;
-                }
-                return Object.assign({}, task, {
-                    title: action.title
-                })
-            });
+            return state.map(task => taskReducer(task, action));
         default :
             return [];
     }
