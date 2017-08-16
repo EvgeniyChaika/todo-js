4e5927feb7671d08d6ff8ed2db25052975d8a28e
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2017-08-16 12:50:22 +0300

    Apply middleware using redux-promise and redux-thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,9 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import promise from 'redux-promise';
+import thunk from 'redux-thunk';
+
 import reducer from '../reducers/reducers';
 
-const store = createStore(reducer);
-
-function addPromiseThunkSupport(store) {
-    const dispatch = store.dispatch;
-
-    return action => {
-        if (typeof action.then === 'function') {
-            return action.then(dispatch);
-        } else if (typeof action === 'function') {
-            return action(dispatch);
-        }
-        return dispatch(action);
-    };
-}
-
-store.dispatch = addPromiseThunkSupport(store);
+const store = createStore(reducer, applyMiddleware(promise, thunk));
 
 export default store;
